089ffcce77057ff0de1f713b521a84701422d00d
Liz Dominguez <liz.dominguez.leon@gmail.com>
2017-07-06 14:37:18 -0400

    added more styling

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ var colors = [
   "rgb(0, 255, 255)",
   "rgb(255, 0, 0)",
   "rgb(255, 255, 0)",
-  "rgb(0, 255, 65)",
+  "rgb(0, 255, 0)",
   "rgb(255, 0, 255)",
-  "rgb(0, 255, 255)"
+  "rgb(250, 50, 50)"
 ]
    
 var squares = document.querySelectorAll(".color");
