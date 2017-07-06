25f7fa030ae917d76290ad4647b7da9774a22aa1
Liz Dominguez <liz.dominguez.leon@gmail.com>
2017-07-06 13:29:25 -0400

    basic user interface

---
+++ main.js
@@ -0,0 +1,24 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+
+  
+var colors = [
+  "rgb(0, 255, 255)",
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 65)",
+  "rgb(255, 0, 255)",
+  "rgb(0, 255, 255)"
+]
+   
+var squares = document.querySelectorAll(".color");
+
+for(var i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+}
+  
+
+  
+
+
+
