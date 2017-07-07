dae735d94ed30db42bf28d66c21bd335bfd4d76b
Liz Dominguez <liz.dominguez.leon@gmail.com>
2017-07-07 17:25:34 -0400

    added more functionality and media queries

--- main.js
+++ main.js
@@ -2,23 +2,75 @@
 /*global $, jQuery*/
 
   
-var colors = [
-  "rgb(0, 255, 255)",
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(255, 0, 255)",
-  "rgb(250, 50, 50)"
-]
+var colors = generateColors(6);
    
-var squares = document.querySelectorAll(".color");
+var circles = document.querySelectorAll(".color");
+var pickedColor = pickColor();
+var colorDisplay = document.querySelector("#colorHeading");
+var messageDisplay = document.getElementById("tryAgain");
+colorDisplay.textContent = pickedColor;
+var newColors = document.getElementById("newColors");
 
-for(var i = 0; i < squares.length; i++){
-    squares[i].style.backgroundColor = colors[i];
+for(var i = 0; i < circles.length; i++){
+  //add initial colors
+    circles[i].style.backgroundColor = colors[i];
+  
+  //add click listeners
+  circles[i].addEventListener("click", function(){
+    //grab color of clicked square & compare to picked color 
+    
+   var clickedColor = this.style.backgroundColor;
+    
+    if (clickedColor === pickedColor) {
+      messageDisplay.textContent = "Correct!";
+      changeColors(pickedColor);
+    }
+    
+    else {
+      //will make wrong colors disappear
+      this.style.backgroundColor = "#fafafa";
+      messageDisplay.textContent = "Try Again";  
+         }
+    
+  });
 }
-  
 
+
+function changeColors(color) {
+  //changes all circles to correct color
+  for(var i = 0; i < circles.length; i++){
+    circles[i].style.backgroundColor = color;
+  } 
   
+}
+
+function pickColor() {
+  //picks a random color
+  var randomNum = Math.floor(Math.random() * colors.length);
+  return colors[randomNum];
+}
+
+function generateColors(num) {
+  //generates all the colors 
+  var arr = [];
+  
+  for(var i = 0; i < num; i++){
+    //get random color and add to array
+    arr.push(randomColor());
+  }
+  
+  return arr;
+}
+
+function randomColor() {
+  //generates random colors: red, green, and blue 
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+  
+}
 
 
 
